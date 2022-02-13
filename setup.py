d7be08f80d12ee7ca8da4183aa4880533d9c12af
Werner-Zeigler <81054771+Werner-Zeigler@users.noreply.github.com>
2022-02-13 11:33:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
   download_url = https://github.com/Werner-Zeigler/PyFetchDB/archive/refs/tags/v2.0.tar.gz',    # I explain this later on
   keywords = ['Python', 'Easy', 'Database'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'ast',
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
