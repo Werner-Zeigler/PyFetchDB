789a7c2fd63f93ee656402867e55f2e8575a5980
Werner-Zeigler <81054771+Werner-Zeigler@users.noreply.github.com>
2022-02-13 11:06:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'PyFetchDB',         # How you named your package folder (MyLib)
-  packages = ['PyFetch'],   # Chose the same as "name"
+  packages = ['PyFetchDB'],   # Chose the same as "name"
   version = '2.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Small, easy to use, python database.',   # Give a short description about your library
